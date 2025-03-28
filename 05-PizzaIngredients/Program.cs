ad8c7fa60e4f994bcbe0b9d53481694ea8d092f6
denisYordanov-eng <denisivov437@gmail.com>
2025-03-28 12:05:04 +0200

    Just made some tasks.

---
+++ 05-PizzaIngredients/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_PizzaIngredients
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] ingrediments = Console.ReadLine().Split(' ').ToArray();
+
+            int lengthOfWords = int.Parse(Console.ReadLine());
+
+
+           var add = new List<string>();
+           byte ingredimentCounter = 0;
+            for (int i = 0; i < ingrediments.Length; i++)
+            {
+
+
+                if (ingrediments[i].Length == lengthOfWords && ingredimentCounter < 10)
+                {
+                    string currentIngrediment = ingrediments[i];
+                    add.Add(currentIngrediment);
+   
+                    Console.WriteLine($"Adding {currentIngrediment}.");
+                    ingredimentCounter++;
+                }
+            }
+            
+            Console.WriteLine($"Made pizza with total of {ingredimentCounter} ingredients.");
+                Console.Write("The ingredients are: {0}.",String.Join(", ",add));
+        }
+    }
+}
---
+++ Problem 4. Grab and Go/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_4.Grab_and_Go
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbersArray = Console.ReadLine()
+                .Split(' ').Select(int.Parse).ToArray();
+
+            int searchNumber = int.Parse(Console.ReadLine());
+
+
+            long sum = 0L;
+            bool numberIsFound = false;
+            int specialCounter = 0;
+            int index = 0;
+            for (int i = numbersArray.Length - 1; i >= 0; i--)
+            {
+                if (numbersArray[i] == searchNumber)
+                {
+                    specialCounter++;
+                    if (specialCounter <= 1)
+                    {
+                        index = i;
+                        numberIsFound = true;
+                        for (int j = 0; j < i; j++)
+                        {
+                            sum += numbersArray[j];
+                        }
+                    }
+                }
+            }
+                if (numberIsFound)
+                {
+                    Console.WriteLine(sum);
+                }
+                else
+                {
+                    Console.WriteLine("No occurrences were found!");
+                }
+
+            
+
+        }
+
+    }
+}
+    
